7958400e409baacdb3a3072f1077620f79c98b76
MD ASH <bma94.7@gmail.com>
2024-02-16 20:53:25 +0000

    Add secret key

--- newstok/settings.py
+++ newstok/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4@5#7efsnxsy*fn_rljy82tkuz97&w$(cgsfz=_27rk-roxpg)'
+#SECRET_KEY = 'django-insecure-4@5#7efsnxsy*fn_rljy82tkuz97&w$(cgsfz=_27rk-roxpg)'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
