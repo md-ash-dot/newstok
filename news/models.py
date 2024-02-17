90e7ebda8cd5549e4b89f7764bfe4feca252d428
MD ASH <bma94.7@gmail.com>
2024-02-17 21:58:34 +0000

    Add meta class to comment

--- news/models.py
+++ news/models.py
@@ -14,6 +14,11 @@ class Article(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
 
 class Comment(models.Model):
     article = models.ForeignKey(
@@ -23,3 +28,8 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
