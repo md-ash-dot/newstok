3e3b400ebce731f1f6ba51196eca9c6e766cd846
MD ASH <bma94.7@gmail.com>
2024-02-17 23:58:16 +0000

    Add generic article view

--- news/views.py
+++ news/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Article
 
 # Create your views here.
+
+
+class ArticleList(generic.ListView):
+    queryset = Article.objects.all()
+    template_name = "article_list.html"
