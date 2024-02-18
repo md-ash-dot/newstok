e972ba9d4e21fc31e0cd41de8a057574bf8e2e77
MD ASH <bma94.7@gmail.com>
2024-02-18 11:31:25 +0000

    Add django-summernote

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Article, Comment
 
+
+@admin.register(Article)
+class ArticleAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
+
 # Register your models here.
-admin.site.register(Article)
 admin.site.register(Comment)
