88242e3202338ff6878f323f7c8732af03c094f2
MD ASH <bma94.7@gmail.com>
2024-02-18 19:39:08 +0000

    added article_detail view

--- news/urls.py
+++ news/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ArticleList.as_view(), name='home'),
+    path('<slug:slug>/', views.article_detail, name='article_detail'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Article
 
@@ -9,3 +9,27 @@ class ArticleList(generic.ListView):
     queryset = Article.objects.filter(status=1)
     template_name = "news/index.html"
     paginate_by = 6
+
+
+def article_detail(request, slug):
+    """
+    Display an individual :model:`article.Post`.
+
+    **Context**
+
+    ``article``
+        An instance of :model:`article.Post`.
+
+    **Template:**
+
+    :template:`news/article_detail.html`
+    """
+
+    queryset = Article.objects.filter(status=1)
+    article = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "news/article_detail.html",
+        {"article": article},
+    )
