d8d00f1d64cc153e806bf2fe8052c54a4f7b76f2
MD ASH <bma94.7@gmail.com>
2024-02-19 22:06:02 +0000

    added comment delete CRUD

--- news/urls.py
+++ news/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('<slug:slug>/', views.article_detail, name='article_detail'),
     path('<slug:slug>/edit_comment/<int:comment_id>',
          views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
 ]
