e2ddefd0f9510594354f995aebc25fc4cc244c95
MD ASH <bma94.7@gmail.com>
2024-02-19 21:55:23 +0000

    edit comments on article as logged in user

--- news/urls.py
+++ news/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.ArticleList.as_view(), name='home'),
     path('<slug:slug>/', views.article_detail, name='article_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
 ]
