eb313ace07632e53360e67d1abc9f7cfb5af6cc9
MD ASH <bma94.7@gmail.com>
2024-02-19 21:34:28 +0000

    Add collaborate form to about page

--- about/admin.py
+++ about/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import About
+from .models import About, CollaborateRequest
 
 # Register your models here.
 
@@ -8,3 +8,8 @@ from .models import About
 class AboutAdmin(SummernoteModelAdmin):
 
     summernote_fields = ('content',)
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+
+    list_display = ('message', 'read',)
