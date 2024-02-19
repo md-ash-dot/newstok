2e0f50daadf794da3d961daf3b5e25cea96a973f
MD ASH <bma94.7@gmail.com>
2024-02-19 14:29:48 +0000

    Add about me section to the website

--- newstok/settings.py
+++ newstok/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-mdashdot-newstok-c5eklmly9kt.ws-eu108.gitpod.io',
                 '.herokuapp.com']
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_summernote',
+    'about',
     'news',
 ]
 
