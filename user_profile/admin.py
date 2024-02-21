83eb2c67cd8e8870303b32c9fe32dc650ead529f
MD ASH <bma94.7@gmail.com>
2024-02-21 16:08:47 +0000

    test

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Post
+
 
 # Register your models here.
+@admin.site.register(Post)
+class AboutAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content',)
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
 
+def user_post(request):
+    """
+    Renders the User Profile page
+    """
+    post = Post.objects.all().order_by('-updated_on').first()
 
+    return render(
+        request,
+        "user_profile/user_profile.html",
+        {"user_profile": user_profile},
+    )
