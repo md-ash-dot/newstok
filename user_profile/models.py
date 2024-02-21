30e1b5d37a65b83cb7271ae8b5ec9990e54c40c8
MD ASH <bma94.7@gmail.com>
2024-02-21 16:19:13 +0000

    removed model

--- user_profile/models.py
+++ user_profile/models.py
@@ -2,13 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="writer")
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    featured_image = CloudinaryField('image', default='placeholder')
-    content = models.TextField()
-    status = models.IntegerField(choices=STATUS, default=0)
-    created_on = models.DateTimeField(auto_now_add=True)
-
-
