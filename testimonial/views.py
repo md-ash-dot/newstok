5120b8c3caf4a368db4825cfff2c4ec9c2971804
MD ASH <bma94.7@gmail.com>
2024-02-23 19:20:08 +0000

    Add testomial view and template

--- testimonial/views.py
+++ testimonial/views.py
@@ -7,3 +7,15 @@ from .models import Testimonial
 class TestimonialList(generic.ListView):
     queryset = Testimonial.objects.filter(status=1)
     template_name = "testimonial_list.html"
+
+def open_testimonial(request):
+    """
+    Renders the Testimonial page
+    """
+    testimonial = Testimonial.objects.all().order_by('-updated_on').first()
+
+    return render(
+        request,
+        "testimonial/testimonial.html",
+        {"testimonial": testimonial},
+    )
