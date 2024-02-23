ce99f8c1f20935db8912a3d1cf9f53e0e039cf83
MD ASH <bma94.7@gmail.com>
2024-02-23 20:06:52 +0000

    edit forms and views in testimonial

--- testimonial/views.py
+++ testimonial/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Testimonial
+from .forms import TestimonialForm
 
 # Create your views here.
 
@@ -8,14 +9,24 @@ class TestimonialList(generic.ListView):
     queryset = Testimonial.objects.filter(status=1)
     template_name = "testimonial_list.html"
 
-def open_testimonial(request):
+def testimonial(request):
     """
     Renders the Testimonial page
     """
-    testimonial = Testimonial.objects.all().order_by('-updated_on').first()
+    if request.method == "POST":
+        testimonial_form = TestimonialForm(data=request.POST)
+        if testimonial_form.is_valid():
+            testimonial = testimonial_form.save(commit=False)
+            testimonial.save()
+
+    """ testimonial = Testimonial.objects.all().order_by('-updated_on').first() """
+    testimonial_form = TestimonialForm()
 
     return render(
         request,
         "testimonial/testimonial.html",
-        {"testimonial": testimonial},
+        {
+            "testimonial": testimonial,
+            "testimonial_form": testimonial_form
+        },
     )
