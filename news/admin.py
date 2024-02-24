da769019d442957017f032fbde81c76592a17c54
MD ASH <bma94.7@gmail.com>
2024-02-24 16:32:07 +0000

    add admin features for comment

--- news/admin.py
+++ news/admin.py
@@ -6,7 +6,7 @@ from .models import Article, Comment
 @admin.register(Article)
 class ArticleAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status', 'created_on', 'category')
+    list_display = ('title', 'status', 'created_on', 'category')
     search_fields = ['title', 'content']
     list_filter = ('status', 'created_on',)
     prepopulated_fields = {'slug': ('title',)}
@@ -14,4 +14,11 @@ class ArticleAdmin(SummernoteModelAdmin):
 
 
 # Register your models here.
-admin.site.register(Comment)
+#admin.site.register(Comment)
+
+@admin.register(Comment)
+class CommentAdmin(SummernoteModelAdmin):
+
+    list_display = ('article', 'author', 'created_on', 'approved',)
+    search_fields = ['article', 'author']
+    list_filter = ('approved', 'created_on',)
